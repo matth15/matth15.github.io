8e0f57b149a6f7f713ab7481b1f154372da20e39
matth15 <mathewsuarez20@gmail.com>
2023-12-01 16:03:39 +0800

    new column added in sql

--- early-alert-system.sql
+++ early-alert-system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2023 at 05:27 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Dec 01, 2023 at 09:01 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `user_type`, `otp`, `otp_expiration`, `is_email_activated`) VALUES
-(1, 'Admin', 'mathewsuarez20@gmail.com', 'admin123123', 'admin', 794415, '2023-11-30 14:37:00', 0),
+(1, 'Admin', 'mathewsuarez20@gmail.com', 'admin123123', 'admin', 309782, '2023-12-01 07:14:00', 0),
 (3, 'admin 2', 'mathewsuarez@gmail.com', 'admin123123', 'admin', 0, '2023-11-29 18:43:49', 0);
 
 -- --------------------------------------------------------
@@ -63,6 +63,7 @@ CREATE TABLE `students_data` (
   `user_type` varchar(50) NOT NULL,
   `otp` int(11) NOT NULL,
   `otp_expiration` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `is_email_activated` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -70,11 +71,11 @@ CREATE TABLE `students_data` (
 -- Dumping data for table `students_data`
 --
 
-INSERT INTO `students_data` (`id`, `unique_id`, `name`, `email`, `password`, `grade_level`, `strand`, `user_type`, `otp`, `otp_expiration`, `is_email_activated`) VALUES
-(1, 0, 'Matthew Suarez', 'msuarez.f2f@tracecollege.edu.ph', '$2y$10$uDjxqVtidz2xBxafgMagHOw3bGU4M4ZHvfhXBj3OUjmGqcnnyxu52', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', 1),
-(2, 0, 'Sedrick Pulutan', 'dpulutan@tracecollege.edu.ph', '$2y$10$f6ON6B4RzRtSWOUWiBvKL.ZM8kzm3tmuO8A/pfmUsdDxKS4rSa6X.', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', 1),
-(39, 0, 'miyuji pogi', 'msasayama.f2f@tracecollege.edu.ph', '$2y$10$pigToHdSL18YaQEBKOE8y.y87Ep9wvlNaWWE3vv94z4PJoqJAyQ96', 'g12', 'gas', 'student', 0, '0000-00-00 00:00:00', 0),
-(40, 6048938, 'Jedrick Murillo', 'jmurillo.f2f@tracecollege.edu.ph', '$2y$10$bK/4rsn0zhJc1G.PeABKTe9k1Q8pARVttQUfdy6tzY4.3sgDzkt9y', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', 1);
+INSERT INTO `students_data` (`id`, `unique_id`, `name`, `email`, `password`, `grade_level`, `strand`, `user_type`, `otp`, `otp_expiration`, `created_at`, `is_email_activated`) VALUES
+(1, 0, 'Matthew Suarez', 'msuarez.f2f@tracecollege.edu.ph', '$2y$10$uDjxqVtidz2xBxafgMagHOw3bGU4M4ZHvfhXBj3OUjmGqcnnyxu52', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', '2023-12-01 14:24:01', 1),
+(2, 0, 'Sedrick Pulutan', 'dpulutan@tracecollege.edu.ph', '$2y$10$f6ON6B4RzRtSWOUWiBvKL.ZM8kzm3tmuO8A/pfmUsdDxKS4rSa6X.', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', '2023-12-01 14:24:01', 1),
+(39, 0, 'miyuji pogi', 'msasayama.f2f@tracecollege.edu.ph', '$2y$10$pigToHdSL18YaQEBKOE8y.y87Ep9wvlNaWWE3vv94z4PJoqJAyQ96', 'g12', 'gas', 'student', 0, '0000-00-00 00:00:00', '2023-12-01 14:24:01', 0),
+(40, 6048938, 'Jedrick Murillo', 'jmurillo.f2f@tracecollege.edu.ph', '$2y$10$bK/4rsn0zhJc1G.PeABKTe9k1Q8pARVttQUfdy6tzY4.3sgDzkt9y', 'g12', 'ict', 'student', 0, '0000-00-00 00:00:00', '2023-12-01 14:24:01', 1);
 
 -- --------------------------------------------------------
 
@@ -84,12 +85,15 @@ INSERT INTO `students_data` (`id`, `unique_id`, `name`, `email`, `password`, `gr
 
 CREATE TABLE `teachers_data` (
   `id` int(11) NOT NULL,
+  `unique_id` int(11) NOT NULL,
   `name` text NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `department` varchar(50) NOT NULL,
   `user_type` text NOT NULL,
   `otp` int(11) NOT NULL,
   `otp_expiration` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `is_email_activated` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -97,9 +101,9 @@ CREATE TABLE `teachers_data` (
 -- Dumping data for table `teachers_data`
 --
 
-INSERT INTO `teachers_data` (`id`, `name`, `email`, `password`, `user_type`, `otp`, `otp_expiration`, `is_email_activated`) VALUES
-(3, 'teacher', 'teacher@tracecollege.edu.ph', 'teacher123123', 'teacher', 897780, '2023-11-30 06:31:00', 1),
-(4, 'teacher 2', 'teacher2@tracecollege.edu.ph', 'teacher123123', 'teacher', 0, '2023-11-29 19:05:20', 1);
+INSERT INTO `teachers_data` (`id`, `unique_id`, `name`, `email`, `password`, `department`, `user_type`, `otp`, `otp_expiration`, `created_at`, `is_email_activated`) VALUES
+(3, 4056405, 'teacher', 'teacher@tracecollege.edu.ph', 'teacher123123', '', 'teacher', 897780, '2023-11-30 06:31:00', '2023-12-01 14:25:04', 1),
+(4, 123451, 'teacher 2', 'teacher2@tracecollege.edu.ph', 'teacher123123', '', 'teacher', 0, '2023-11-29 19:05:20', '2023-12-01 14:25:04', 1);
 
 --
 -- Indexes for dumped tables
